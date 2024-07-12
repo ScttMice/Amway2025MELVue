e714f19b75d9f286a3e221dc7d66fde128249655
MTS\1833 <beanai163@163.com>
2024-07-12 09:20:57 +0800

    1

--- src/views/info-auth/index.vue
+++ src/views/info-auth/index.vue
@@ -10,12 +10,14 @@
 
         <div class="page-footer pd24">
             <div class="mg-b20">
-                <van-checkbox :disabled="soll < 200" style="line-height: 36px;" icon-size="16px" shape="square"
-                    v-model="checked">
+                <van-checkbox style="line-height: 36px;" icon-size="16px" shape="square" v-model="checked">
+                    <!-- <van-checkbox :disabled="soll < 200" style="line-height: 36px;" icon-size="16px" shape="square"
+                        v-model="checked"> -->
                     <span class="font14" style="vertical-align: middle;">我已经阅读并同意以上授权</span>
                 </van-checkbox>
             </div>
-            <van-button block type="primary" loading-type="circular" loading-size="24px" :disabled="!checked">
+            <van-button @click="toNext" block type="primary" loading-type="circular" loading-size="24px"
+                :disabled="!checked">
                 下一步
             </van-button>
         </div>
@@ -24,14 +26,22 @@
 
 <script lang="ts" setup>
 import { ref, reactive } from "vue";
+import { useRouter } from "vue-router";
 
-
+const router = useRouter();
 const soll = ref<number>(0);
 const checked = ref<boolean>(false);
 
 
 const onScroll = (e: Event) => {
     console.log(e);
+}
+
+
+const toNext = () => {
+    router.push({
+        name: 'person'
+    })
 }
 </script>
 
